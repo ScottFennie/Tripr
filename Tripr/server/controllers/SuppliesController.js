a59d704f9c6388581d12e7b161fd437aeccb1241
nathan <nateohendricks@gmail.com>
2021-10-07 14:56:35 -0600

    supplies controller and service

---
+++ Tripr/server/controllers/SuppliesController.js
@@ -0,0 +1,62 @@
+import BaseController from '../utils/BaseController'
+import { Auth0Provider } from '@bcwdev/auth0provider'
+import { suppliesService } from '../services/SuppliesService'
+
+export class SuppliesController extends BaseController {
+  constructor() {
+    super('api/trips/:tripId/supplies')
+    this.router
+      .use(Auth0Provider.getAuthorizedUserInfo)
+      .get('/:suppliesId', this.getSupply)
+      .get('', this.getSupplies)
+      .post('', this.createSupplies)
+      .delete('/:suppliesId', this.removeSupplies)
+      .put('/:suppliesId', this.editSupplies)
+  }
+
+  async getSupplies(req, res, next) {
+    try {
+      const supply = await suppliesService.getSupplies(req.query)
+      res.send(supply)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async getSupply(req, res, next) {
+    try {
+      const supply = await suppliesService.getSupplyById(req.params.suppliesId)
+      res.send(supply)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async createSupplies(req, res, next) {
+    try {
+      req.body.creatorId = req.userInfo.id
+      const supply = await suppliesService.createSupplies(req.body)
+      res.send(supply)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async removeSupplies(req, res, next) {
+    try {
+      const supply = await suppliesService.removeSupplies(req.params.suppliesId, req.userInfo.id)
+      res.send(supply)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async editSupplies(req, res, next) {
+    try {
+      const supply = await suppliesService.editSupplies(req.params.suppliesId, req.userInfo.id, req.body)
+      res.send(supply)
+    } catch (error) {
+      next(error)
+    }
+  }
+}
---
+++ Tripr/server/services/SuppliesService.js
@@ -0,0 +1,44 @@
+import { dbContext } from '../db/DbContext'
+import { BadRequest, Forbidden } from '../utils/Errors'
+
+class SuppliesService {
+  async getSupplyById(suppliesId) {
+    const supply = await dbContext.Supplies.findById(suppliesId).populate('creator')
+    if (!supply) {
+      throw new BadRequest('invalid Id')
+    }
+    return supply
+  }
+
+  async getSupplies(query) {
+    const supply = await dbContext.Supplies.find(query).populate('creator')
+    return supply
+  }
+
+  async createSupplies(suppliesData) {
+    const supply = await dbContext.Supplies.create(suppliesData)
+    await supply.populate('creator')
+    return supply
+  }
+
+  async removeSupplies(suppliesId, userId) {
+    const supply = await this.getSupplyById(suppliesId)
+    if (userId !== supply.creatorId.toString()) {
+      throw new Forbidden('you cant do that')
+    }
+    await supply.remove()
+    return supply
+  }
+
+  async editSupplies(suppliesId, userId, suppliesData) {
+    const supply = await this.getSupplyById(suppliesId)
+    if (userId !== supply.creatorId.toString()) {
+      throw new Forbidden('you cant do that')
+    }
+    supply.description = suppliesData.description || supply.description
+    supply.isBringing = suppliesData.isBringing || supply.isBringing
+    await supply.save()
+    return supply
+  }
+}
+export const suppliesService = new SuppliesService()

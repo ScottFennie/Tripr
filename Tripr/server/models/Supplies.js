0f6c56a2bd2b95e5469615548c8b261270cddaea
nathan <nateohendricks@gmail.com>
2021-10-07 13:50:37 -0600

    supplies schema is filled out

--- Tripr/server/models/Supplies.js
+++ Tripr/server/models/Supplies.js
@@ -1,5 +1,29 @@
-export class Supplies {
-  constructor(suppliesData) {
-    this.name = suppliesData.name
-  }
-}
+import mongoose from 'mongoose'
+const Schema = mongoose.Schema
+
+export const SuppliesSchema = new Schema({
+  description: { type: String, required: true },
+  isBringing: { type: Boolean, default: false },
+  tripId: { type: Schema.Types.ObjectId, required: true, ref: 'Trip' },
+  creatorId: { type: Schema.Types.ObjectId, required: true, ref: 'Account' },
+  travlerId: { type: Schema.Types.ObjectId, required: true, ref: 'Travler' }
+}, { timestamps: true, toJSON: { virtuals: true } })
+
+SuppliesSchema.virtual('trip', {
+  localField: 'tripId',
+  foreignField: '_id',
+  ref: 'Trip',
+  justOne: true
+})
+SuppliesSchema.virtual('creator', {
+  localField: 'creatorId',
+  foreignField: '_id',
+  ref: 'Account',
+  justOne: true
+})
+SuppliesSchema.virtual('traveler', {
+  localField: 'travelerId',
+  foreignField: '_id',
+  ref: 'Traveler',
+  justOne: true
+})

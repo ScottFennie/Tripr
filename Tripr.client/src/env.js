2da91ead1514b7a2e5ebdaef32f72d72228c2572
Jackson Schacher <jackson.schacher@gmail.com>
2021-10-07 11:47:57 -0600

    no more homepage and env

--- Tripr.client/src/env.js
+++ Tripr.client/src/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'tripr.us.auth0.com'
+export const clientId = 'Jv4nlGPzkXbv9lMsGAExjdcYV1kTMezI'
+export const audience = 'https://tripr.us.auth0.com/api/v2/'

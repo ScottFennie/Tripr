84459fbd4c83a98c95a9e4c468a3f691051fad31
Quinnton <quinnclimbs@gmail.com>
2021-10-07 13:58:32 -0600

    reoute detaisl contorller and service updated

---
+++ Tripr/server/controllers/RouteDetailsController.js
@@ -0,0 +1,51 @@
+import { routeDetailsService } from '../services/RouteDetailsService'
+import BaseController from '../utils/BaseController'
+
+export class RouteDetailsController extends BaseController {
+  constructor() {
+    super('api/trips/:tripId/routes')
+    this.router
+      .get('', this.getRouteDetails)
+      .put('/id', this.editRouteDetails)
+      .post('', this.createRouteDeatils)
+      .delete('/id', this.removeRouteDetails)
+  }
+
+  async removeRouteDetails(req, res, next) {
+    try {
+      const route = await routeDetailsService.removeRouteDetails(req.params.id)
+      res.send(route)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async createRouteDeatils(req, res, next) {
+    try {
+      req.body.creatorId = req.userInfo.id
+      req.body.tripId = req.params.tripId
+      const route = await routeDetailsService.createRouteDeatils(req.body)
+      res.send(route)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async editRouteDetails(req, res, next) {
+    try {
+      const route = await routeDetailsService.editRouteDetails(req.body)
+      res.send(route)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async getRouteDetails(req, res, next) {
+    try {
+      const route = await routeDetailsService.getRouteDetails(req.params.id)
+      res.send(route)
+    } catch (error) {
+      next(error)
+    }
+  }
+}

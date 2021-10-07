d55982fe9ad11ab450e6f49a8a0eb59668855b83
Jackson Schacher <jackson.schacher@gmail.com>
2021-10-07 12:02:08 -0600

    probably broke casue i didn't cjheck first

---
+++ Tripr.client/src/pages/LoginPage.vue
@@ -0,0 +1,37 @@
+<template>
+  <button
+    class="btn selectable text-success lighten-30 text-uppercase my-2 my-lg-0"
+    @click="login"
+    v-if="!user.isAuthenticated"
+  >
+    Login
+  </button>
+  <footer>
+    <div class="bg-dark text-light text-center p-4">
+      Application Created by Nathan, Quinn, Scott, Drew, Jackson 2021
+    </div>
+  </footer>
+</template>
+
+<script>
+import { computed } from '@vue/runtime-core'
+import { AppState } from '../AppState'
+import { AuthService } from '../services/AuthService'
+export default {
+  setup() {
+    return {
+      user: computed(() => AppState.user),
+      async login() {
+        AuthService.loginWithPopup()
+      },
+      async logout() {
+        AuthService.logout({ returnTo: window.location.origin })
+      }
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
--- Tripr.client/src/router.js
+++ Tripr.client/src/router.js
@@ -8,8 +8,8 @@ function loadPage(page) {
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: loadPage('HomePage')
+    name: 'Login',
+    component: loadPage('LoginPage')
   },
   {
     path: '/about',

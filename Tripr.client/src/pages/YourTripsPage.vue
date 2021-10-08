934430b2945c69d9f35f9563d3b4abaa8aa821eb
Scott Fennie <s.fennie7@gmail.com>
2021-10-08 10:27:10 -0600

    routes done

---
+++ Tripr.client/src/pages/YourTripsPage.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container-fluid bg-primary">
+    <div class="row">
+      <div class="col-12 d-flex justify-content-center">
+        <div class="text-shadow2 text-center">
+          <h2 class="">
+            Trips
+          </h2>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+.text-shadow2 {
+  border-radius: 10px;
+  padding: 1rem 1rem;
+  border: 3px dashed rgba(255,255,255,0.5);
+  color: #CCA363;
+  background-color: #E7DEBE;
+    text-shadow: 0px 2px 2px rgba(255,255,255,0.5);
+    width: 50vw;
+    height: 10vh;
+}
+
+.head-he
+</style>

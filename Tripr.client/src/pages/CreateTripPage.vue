eb4cde714b2052f5d13ef39a4186e1125bf1d2e0
drewmcreynolds <drew.gmcreynolds@gmail.com>
2021-10-08 10:20:47 -0600

    create trips page form

---
+++ Tripr.client/src/pages/CreateTripPage.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="container-fluid p-0">
+    <div class="row justify-content-center">
+      <form>
+        <div class="mb-3">
+          <label for="title" class="form-label">Title</label>
+          <input type="title" class="form-control" id="title">
+        </div>
+        <div class="mb-3">
+          <label for="location" class="form-label">Location</label>
+          <input type="location" class="form-control" id="location">
+        </div>
+        <select class="form-select" aria-label="Travel type">
+          <option selected>
+            Select you Travel Type
+          </option>
+          <option value="Foot">
+            Foot
+          </option>
+          <option value="Road">
+            Road
+          </option>
+          <option value="Camping">
+            Camping
+          </option>
+        </select>
+
+        <button type="submit" class="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>

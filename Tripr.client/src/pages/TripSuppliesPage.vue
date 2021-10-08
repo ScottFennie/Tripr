194802efcbbc65053cedb08b6edef94403570653
nathan <nateohendricks@gmail.com>
2021-10-08 11:34:47 -0600

    tripSupplies page

--- Tripr.client/src/pages/TripSuppliesPage.vue
+++ Tripr.client/src/pages/TripSuppliesPage.vue
@@ -6,20 +6,29 @@
       </div>
       <div class="col-md-8 d-flex align-items-start justify-content-end">
         <h1>Supplies</h1>
-        <button title="Add Supplies" class="btn">
+        <button title="Add Supplies" class="btn" data-bs-toggle="modal" data-bs-target="#supply-modal">
           <i class="mdi mdi-plus"></i>
         </button>
       </div>
     </div>
     <div class="row">
       <h1>Needed:</h1>
-      <!-- inject supplies list here -->
+      <SuppliesList :key="s.id" v-for="s in supplies" :supply="s" />
     </div>
     <div class="row">
       <h1>Assigned:</h1>
       <!-- inject Assigned list here -->
     </div>
   </div>
+
+  <Modal id="supply-modal">
+    <template #modal-title>
+      <h3>Supplies</h3>
+    </template>
+    <template #modal-body>
+      <SuppliesForm />
+    </template>
+  </Modal>
 </template>
 
 <script>

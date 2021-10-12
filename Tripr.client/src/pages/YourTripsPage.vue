e3cabfee98d490ea329622d61f1825f880b81252
Jackson Schacher <jackson.schacher@gmail.com>
2021-10-12 13:38:37 -0600

    Trips updated

--- Tripr.client/src/pages/YourTripsPage.vue
+++ Tripr.client/src/pages/YourTripsPage.vue
@@ -4,46 +4,21 @@
       <div class="col-12">
         <div class="d-flex justify-content-end">
           <router-link :to="{ name: 'Account' }" class="">
-            <h2 class="selectable t-shad3">
-              x
-            </h2>
+            <i class="mdi mdi-close selectable f-26 close-btn"></i>
           </router-link>
         </div>
         <div class="d-flex justify-content-center my-0">
-          <div class="text-shadow2 t-shad2 d-flex justify-content-center align-items-center">
-            <h1 class="py-0">
+          <div class="trip-patch d-flex justify-content-center align-items-center">
+            <h1 class="trip-border">
               Trips
             </h1>
           </div>
         </div>
       </div>
     </div>
-    <div class="row d-flex justify-content-center row-h">
-      <TripCard :trip="t" v-for="t in trips" :key="t.id" />
-    </div>
-    <div class="row bg-primary">
-      <div class="col-12 d-flex justify-content-center box-bottom-shadow rounded p-0" v-if="trips.length >= 2">
-        <i class=" t-shad3 mdi mdi-menu-down f-30 "></i>
-      </div>
-      <div class="col-12 d-flex flex-column">
-        <div class="text-center">
-          <p class="mt-2 mb-0 t-shad3">
-            Past trips
-          </p>
-        </div>
-        <div class="text-center t-shad3">
-          <h2>Summer Trip</h2>
-        </div>
-        <div class="text-center body-color">
-          <h5>June 6, 2021</h5>
-        </div>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-12">
-        <div class="card shadow">
-          <Map />
-        </div>
+    <div class="row d-flex justify-content-center">
+      <div class="accordion accordion-flush" id="accordionFlushExample">
+        <TripCard :trip="t" v-for="t in trips" :key="t.id" />
       </div>
     </div>
   </div>
@@ -64,23 +39,38 @@ export default {
 </script>
 
 <style lang="scss">
+.trip-patch {
+  box-shadow: 3px 3px 5px #A28558;
+  margin: .25rem;
+  padding: .25vh .5vw;
+  border-radius: 10px;
+  background-color: #e7debe;
+}
+.trip-border {
+  border: 2px dashed #fff5d3;
+  padding: .5vh 20vw;
+  margin-bottom: 0;
+  border-radius: 10px;
+  font-family: museo-slab,serif;
+  font-weight: bold;
+  color: #A28558;
+}
 .text-shadow2 {
   border-radius: 10px;
   padding: 1rem 1rem;
-  border: 3px dashed rgba(255,255,255,0.5);
-  color: #CCA363;
+  border: 2px dashed rgb(226, 188, 127);
+  color: rgb(204, 163, 99);
   background-color: #E7DEBE;
     text-shadow: 0px 2px 2px rgba(255,255,255,0.5);
     width: 50vw;
     height: 7vh;
 }
 .trip-card {
+  font-family: museo-slab,serif;
   border-radius: 10px;
   padding: .5rem 1rem;
-  border: 3px dashed rgba(255,255,255,0.5);
-  color: #A28558;
+  color: #6e5a3c;
   background-color: #CCA363;
-    text-shadow: 0px 2px 2px rgba(255,255,255,0.5);
     width: 85vw;
     height: 6vh;
 }
@@ -112,5 +102,8 @@ text-shadow: 0 1px 2px rgba(255, 255, 255, 0.4);
 .row-h{
     max-height: 20vh;
     overflow: auto;
+}
+.close-btn {
+  color: #A28558;
 }
 </style>

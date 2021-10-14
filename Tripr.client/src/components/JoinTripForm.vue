82e5506dee228964a0ed27fb45d60ab81b6cfaa7
drewmcreynolds <drew.gmcreynolds@gmail.com>
2021-10-14 10:29:48 -0600

    join trip modal changes

--- Tripr.client/src/components/JoinTripForm.vue
+++ Tripr.client/src/components/JoinTripForm.vue
@@ -2,7 +2,7 @@
   <form @submit.prevent="joinTrip()">
     <div class="form-group">
       <label for="tripCode"></label>
-      <input name="tripCode" type="text" v-model="editable.jkey">
+      <input class="bg-primary w-100 p-4 bord" name="tripCode" type="text" v-model="editable.jkey">
     </div>
   </form>
 </template>
@@ -29,5 +29,13 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.text-shadow {
+  border-radius: 3px;
+  padding: .15rem 1rem;
+  border: 3px dashed rgba(255,255,255,0.5);
+  color: #A28558;
+  text-shadow: 0px 2px 2px rgba(255,255,255,0.5);
+}
 
+.bord{    border-color: #E62D90;    border-style: dashed; }
 </style>
--- Tripr.client/src/components/Modal.vue
+++ Tripr.client/src/components/Modal.vue
@@ -2,13 +2,13 @@
   <div class=" modal fade" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered">
       <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">
+        <div class="modal-header bg-body">
+          <h5 class="modal-title bg-body" id="exampleModalLabel">
             <slot name="modal-title"></slot>
           </h5>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
-        <div class="modal-body">
+        <div class="modal-body bg-body rounded">
           <slot name="modal-body"></slot>
         </div>
       </div>

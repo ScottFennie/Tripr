9df378ae5c367f42f41c1b49098a071b4d5b51b8
Scott Fennie <s.fennie7@gmail.com>
2021-10-14 11:38:08 -0600

    map doneish

--- Tripr.client/src/components/MapBox.vue
+++ Tripr.client/src/components/MapBox.vue
@@ -1,5 +1,7 @@
 <template>
-  <div id="map"></div>
+  <div>
+    <div id="map"></div>
+  </div>
 </template>
 
 <script>
@@ -18,51 +20,65 @@ export default {
       })
 
       map.on('load', () => {
-        // Add an image to use as a custom marker
+        const geocoder = new MapboxGeocoder({
+          // Initialize the geocoder
+          accessToken: mapboxGl.accessToken, // Set the access token
+          mapboxgl: mapboxGl, // Set the mapbox-gl instance
+          marker: false, // Do not use the default marker style
+          placeholder: 'Search for a starting point', // Placeholder text for the search bar
+          position: 'bottom-left'
+        })
+        map.addControl(geocoder, 'bottom-left')
+
         map.loadImage(
           'https://i.postimg.cc/HWPXn5VJ/pin-small.png',
           (error, image) => {
             if (error) throw error
             map.addImage('custom-marker', image)
             // Add a GeoJSON source with 2 points
-            map.addSource('points', {
+            // map.addSource('points', {
+            //   type: 'geojson',
+            //   data: {
+            //     type: 'FeatureCollection',
+            //     features: [
+            //       {
+            //         // feature for Mapbox DC
+            //         type: 'Feature',
+            //         geometry: {
+            //           type: 'Point',
+            //           coordinates: [
+            //             -77.03238901390978, 38.913188059745586
+            //           ]
+            //         },
+            //         properties: {
+            //           title: 'Mapbox DC'
+            //         }
+            //       },
+            //       {
+            //         // feature for Mapbox SF
+            //         type: 'Feature',
+            //         geometry: {
+            //           type: 'Point',
+            //           coordinates: [-122.414, 37.776]
+            //         },
+            //         properties: {
+            //           title: 'Tripr SF'
+            //         }
+            //       }
+            //     ]
+            //   }
+            // })
+            map.addSource('single-point', {
               type: 'geojson',
               data: {
                 type: 'FeatureCollection',
-                features: [
-                  {
-                    // feature for Mapbox DC
-                    type: 'Feature',
-                    geometry: {
-                      type: 'Point',
-                      coordinates: [
-                        -77.03238901390978, 38.913188059745586
-                      ]
-                    },
-                    properties: {
-                      title: 'Mapbox DC'
-                    }
-                  },
-                  {
-                    // feature for Mapbox SF
-                    type: 'Feature',
-                    geometry: {
-                      type: 'Point',
-                      coordinates: [-122.414, 37.776]
-                    },
-                    properties: {
-                      title: 'Mapbox SF'
-                    }
-                  }
-                ]
+                features: []
               }
             })
-
-            // Add a symbol layer
             map.addLayer({
-              id: 'points',
+              id: 'point',
               type: 'symbol',
-              source: 'points',
+              source: 'single-point',
               layout: {
                 'icon-image': 'custom-marker',
                 // get the title name from the source's "title" property
@@ -74,6 +90,13 @@ export default {
                 'text-offset': [0, 1.25],
                 'text-anchor': 'top'
               }
+            })
+
+            // Listen for the `result` event from the Geocoder
+            // `result` event is triggered when a user makes a selection
+            //  Add a marker at the result's coordinates
+            geocoder.on('result', ({ result }) => {
+              map.getSource('single-point').setData(result.geometry)
             })
           }
         )
@@ -91,8 +114,14 @@ export default {
 #map {
   height: 40vh;
 }
-.mapbox-improve-map {
+.mapboxgl-ctrl-attrib-inner {
+    display: none;
+}
+.mapboxgl-ctrl.mapboxgl-ctrl-attrib{
     display: none;
 }
 
+.mapboxgl-ctrl-geocoder--input{
+
+}
 </style>

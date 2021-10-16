dda8c394b53d89ae4af390dc409edf040f0b9ae2
Jackson Schacher <jackson.schacher@gmail.com>
2021-10-15 19:28:26 -0600

    save?

--- Tripr.client/src/AppState.js
+++ Tripr.client/src/AppState.js
@@ -4,31 +4,7 @@ import { reactive } from 'vue'
 export const AppState = reactive({
   user: {},
   account: {},
-  currentTrip: {
-    title: "The Boi's Trip",
-    travelType: 'Camping',
-    id: 1,
-    geo: {
-      type: 'geojson',
-      data: {
-        type: 'FeatureCollection',
-        features: [
-          {
-            id: 'neighborhood.9617859997698260',
-            type: 'Feature',
-            text: 'Zimbabwe',
-            geometry: {
-              type: 'Point',
-              coordinates: [
-                30.976555,
-                -29.691168
-              ]
-            }
-          }
-        ]
-      }
-    }
-  },
+  currentTrip: {},
   trips: [],
   currenttrip: [],
   currentTripId: null,
@@ -43,39 +19,6 @@ export const AppState = reactive({
   profile: null,
   mytrips: [],
   tripMapSource: {},
-  tripStartSource: {},
-  trial: {
-    id: 'my-fake-id',
-    type: 'geojson',
-    data: {
-      type: 'FeatureCollection',
-      features: [
-        {
-          id: 'place.15042767980230180',
-          type: 'Feature',
-          text: 'Boise',
-          geometry: {
-            type: 'Point',
-            coordinates: [
-              -116.2044,
-              43.615
-            ]
-          }
-        },
-        {
-          id: 'place.9607189446701850',
-          type: 'Feature',
-          text: 'Chicago',
-          geometry: {
-            type: 'Point',
-            coordinates: [
-              -87.6244,
-              41.8756
-            ]
-          }
-        }
-      ]
-    }
-  }
+  tripStartSource: {}
 
 })

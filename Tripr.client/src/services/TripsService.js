1695e84fc036e5c2aad38d1a97bb1d7d155a3b17
Scott Fennie <s.fennie7@gmail.com>
2021-10-15 20:12:16 -0600

    pesh

--- Tripr.client/src/services/TripsService.js
+++ Tripr.client/src/services/TripsService.js
@@ -48,7 +48,7 @@ class TripsService {
     AppState.trips = res.data.map(t => new Trip(t))
     const trip = AppState.trips.find(t => t.id === tripID)
     AppState.currentTrip = trip
-    // AppState.tripMapSource = trip.geo
+    AppState.tripMapSource = trip.geo
     logger.log('myID', trip)
   }
 
